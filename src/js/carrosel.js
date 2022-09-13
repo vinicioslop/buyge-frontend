dca51a3452a2a01b9d0733e8765dad38404e0fc1
vinicioslop <vinicioslop7@hotmail.com>
2022-09-13 11:00:29 -0300

    Carrosel com atualização automatica, faltando mudar animação e imagens

--- src/js/carrosel.js
+++ src/js/carrosel.js
@@ -9,22 +9,16 @@ function currentSlide(n) {
     showSlides((slideIndex = n));
 }
 
-function showSlides(n) {
+function showSlides() {
     let i;
     let slides = document.getElementsByClassName("mySlides");
-    let dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {
-        slideIndex = 1;
-    }
-    if (n < 1) {
-        slideIndex = slides.length;
-    }
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
     }
     slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
+    setTimeout(showSlides, 5000); // Change image every 2 seconds
 }
